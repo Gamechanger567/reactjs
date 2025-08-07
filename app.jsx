4acf4011234993d5645616e31f29218381849931
Subham Modi <modisubham1709@gmail.com>
2025-08-07 12:50:56 +0530

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,13 @@
+function App(){
+    return(
+        <>
+            <Greet Name="Praveer Ranjan Singh"/>
+            <Greet Name="AYUSH"/>
+            <Greet Name="Srijita"/>
+            <Greet Name="Shreya"/>
+            <Greet Name="SAM"/>
+        </>
+    )
+
+}
+export default App
